4f12c9b666aff97703f8cd9433632cb3fe9ecaee
Polina <reisikh.po@phystech.edu>
2022-12-15 06:32:58 +0300

    velocity distribution histogram added

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include <vector>
+#include <fstream>
+#include <cmath>
 
 using namespace std;
 
@@ -13,6 +15,8 @@ const int SCREEN_WIDTH = 1000;
 const int SCREEN_HEIGHT = 800;
 
 int main() {
+    int timer = 5000;
+
     Update_Function game(vector<float>({0,0, float(SCREEN_WIDTH), float(SCREEN_HEIGHT)}), 0.001);
 
     game.Create_Pool(100, 10, true, 10);
@@ -30,21 +34,30 @@ int main() {
 
     sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "gas", sf::Style::Default);
 
-    sf::RenderWindow graph(sf::VideoMode(200, 100), "graph", sf::Style::Titlebar);
-
     while (window.isOpen()) {
         sf::Event event{};
         while (window.pollEvent(event)) {
-
             if (event.type == sf::Event::Closed) {
                 window.close();
-                graph.close();
             }
-
-
-
         }
         window.clear();
+
+        ofstream output;
+
+        if (timer == 5000) {
+            timer = 0;
+            output.open("output.csv");
+            for (auto &ball : game.vector_of_balls) {
+                output << sqrt(ball.vx * ball.vx +
+                               ball.vy * ball.vy) << "\n";
+                cout << sqrt(ball.vx * ball.vx +
+                             ball.vy * ball.vy) << " ";
+            }
+            output.close();
+        }
+
+        timer += 1;
 
         for (int i = 0; i < game.vector_of_balls.size(); i++) {
             ball_graph[i].setPosition(game.vector_of_balls[i].x - game.vector_of_balls[i].radius,
@@ -55,8 +68,6 @@ int main() {
         game.Update_Balls();
         window.display();
     }
-
-
 
     return 0;
 }
